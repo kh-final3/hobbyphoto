57a4016bec477c8411fde0d61f27ae78620a5484
hoyeon <aviationlovers@naver.com>
2023-10-16 02:56:35 +0900

    Feat : 게시판 기능 추가

--- hobbyphoto/src/main/java/com/kh/hobbyphoto/common/model/vo/PageInfo.java
+++ hobbyphoto/src/main/java/com/kh/hobbyphoto/common/model/vo/PageInfo.java
@@ -1,5 +1,24 @@
 package com.kh.hobbyphoto.common.model.vo;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
 public class PageInfo {
+	private int listCount;
+	private int currentPage;
+	private int pageLimit;
+	private int boardLimit;
+	
+	private int maxPage;
+	private int startPage;
+	private int endPage;
 
 }
