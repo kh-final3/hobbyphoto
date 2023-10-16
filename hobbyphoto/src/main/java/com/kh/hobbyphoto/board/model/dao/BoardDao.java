df2e3174da1d32fafdb08c16cd15f7c0c8cc6764
hy <aviationlovers@naver.com>
2023-10-16 09:58:01 +0900

    Feat: boardList 1차

--- hobbyphoto/src/main/java/com/kh/hobbyphoto/board/model/dao/BoardDao.java
+++ hobbyphoto/src/main/java/com/kh/hobbyphoto/board/model/dao/BoardDao.java
@@ -1,8 +1,58 @@
 package com.kh.hobbyphoto.board.model.dao;
 
+import java.util.ArrayList;
+
+import org.apache.ibatis.session.RowBounds;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
+
+import com.kh.hobbyphoto.board.model.vo.Board;
+import com.kh.hobbyphoto.board.model.vo.Reply;
+import com.kh.hobbyphoto.common.model.vo.PageInfo;
 
 @Repository
 public class BoardDao {
+	public int selectListCount(SqlSessionTemplate sqlSession) {
+		return sqlSession.selectOne("boardMapper.selectListCount");
+	}
 
+	public ArrayList<Board> selectList(SqlSessionTemplate sqlSession, PageInfo pi) {
+		int offset = (pi.getCurrentPage() - 1) * pi.getBoardLimit();
+		int limit = pi.getBoardLimit();
+		
+		RowBounds rowBounds = new RowBounds(offset, limit);
+		return (ArrayList)sqlSession.selectList("boardMapper.selectList", null, rowBounds);
+	}
+
+	public int insertBoard(SqlSessionTemplate sqlSession, Board b) {
+		return sqlSession.insert("boardMapper.insertBoard", b);
+	}
+
+	public int increaseCount(SqlSessionTemplate sqlSession, int boardNo) {
+		return sqlSession.update("boardMapper.increaseCount", boardNo);
+	}
+
+	public Board selectBoard(SqlSessionTemplate sqlSession, int boardNo) {
+		return (Board) sqlSession.selectOne("boardMapper.selectBoard", boardNo);
+	}
+
+	public int deleteBoard(SqlSessionTemplate sqlSession, int boardNo) {
+		return sqlSession.update("boardMapper.deleteBoard", boardNo);
+	}
+
+	public int updateBoard(SqlSessionTemplate sqlSession, Board b) {
+		return sqlSession.update("boardMapper.updateBoard", b);
+	}
+
+	public ArrayList<Reply> selectReplyList(SqlSessionTemplate sqlSession, int boardNo) {
+		return (ArrayList) sqlSession.selectList("boardMapper.selectReplyList", boardNo);
+	}
+
+	public int insertReply(SqlSessionTemplate sqlSession, Reply r) {
+		return sqlSession.insert("boardMapper.insertReply", r);
+	}
+	
+	public ArrayList<Board> selectTopBoardList(SqlSessionTemplate sqlSession){
+		return (ArrayList) sqlSession.selectList("boardMapper.selectTopBoardList");
+	}
 }
--- hobbyphoto/src/main/java/com/kh/hobbyphoto/board/model/service/BoardService.java
+++ hobbyphoto/src/main/java/com/kh/hobbyphoto/board/model/service/BoardService.java
@@ -1,5 +1,38 @@
 package com.kh.hobbyphoto.board.model.service;
 
+import java.util.ArrayList;
+
+import com.kh.hobbyphoto.board.model.vo.Board;
+import com.kh.hobbyphoto.board.model.vo.Reply;
+import com.kh.hobbyphoto.common.model.vo.PageInfo;
+
 public interface BoardService {
 //서비스 설계하는 곳
+	
+	// 1. 게시판 리스트 페이지 서비스 (페이징)
+	int selectListCount();
+	ArrayList<Board> selectList(PageInfo pi);
+	
+	// 2. 게시글 작성하기 서비스
+	int insertBoard(Board b);
+	
+	// 3. 게시글 상세조회용 서비스
+	int increaseCount(int boardNo);
+	Board selectBoard(int boardNo);
+	
+	// 4. 게시글 삭제용 서비스
+	int deleteBoard(int boardNo);
+	
+	// 5. 게시글 수정용 서비스
+	int updateBoard(Board b);
+
+	// 6. 댓글 리스트 조회용 서비스(ajax)
+	ArrayList<Reply> selectReplyList(int boardNo); 
+
+	// 7. 댓글 작성용 서비스(ajax)
+	int insertReply(Reply r);
+	
+	// 8. 조회수 top5 리스트 조회용 서비스
+	ArrayList<Board> selectTopBoardList();
+	
 }
